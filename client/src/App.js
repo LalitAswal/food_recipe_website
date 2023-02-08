bf3f317e71f6f8011eb84e4a31497757783056d6
LalitAswal <LalitAswal143@gmail.com>
2023-02-08 17:28:39 +0530

    added react compoment for signup

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import Home from "./component/Home";
 import Recipes from "./component/RecipeList";
 import FoodDetails from "./component/FoodDetails";
 import Login from './component/Login';
+import SighUp  from './component/Signup';
 
 
 
@@ -11,9 +12,10 @@ const App = () =>{
   return (
     <div className="App">
       <Navbar/>
-
+      
       <Routes>
       <Route exact  path="/"      element ={ <Login/>}/>
+      <Route exact  path="/signup"      element ={ <SighUp/>}/>
         <Route exact  path="/home"      element ={ <Home/>}/>
         <Route   path="/RecipeList"   element = { <Recipes/> } />
         <Route   path="/FoodDetails"   element = { <FoodDetails/> } />
