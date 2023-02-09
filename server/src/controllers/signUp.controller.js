5662a92aa2a07d2ee6cb33036f3fbbe83d7de0a0
LalitAswal <LalitAswal143@gmail.com>
2023-02-09 21:57:52 +0530

    code clean

--- server/src/controllers/signUp.controller.js
+++ server/src/controllers/signUp.controller.js
@@ -3,9 +3,9 @@ const signUpModel = require('../models/signUp');
 
 const  signUp = async(req, res) =>{
     try {
-        console.log('checking controller')
+
     let { id,name, user_id, password, verify_password, email} = req.body;
-    console.log(req.body);
+    
     let data = await signUpModel(id,name, user_id, password, verify_password, email);
     res.status(200).json(data);
     } catch (error) {
